50715eefbd898bbd7e09ae028ac4c53204ab78b0
amannagle <aman.nagle1999@gmail.com>
2024-06-18 14:39:57 +0530

    added find by id functionality

--- index.js
+++ index.js
@@ -1,24 +1,28 @@
 const express = require("express");
-require('dotenv').config()
-const Person = require('./models/phonebook')
+require("dotenv").config();
+const Person = require("./models/phonebook");
 const app = express();
-const cors = require('cors');
+const cors = require("cors");
 app.use(cors());
-app.use(express.static('dist'));
-const morgan = require('morgan');
-morgan.token('body', function (req, res) { return JSON.stringify(req.body?req.body:"") })
+app.use(express.static("dist"));
+const morgan = require("morgan");
+morgan.token("body", function (req, res) {
+  return JSON.stringify(req.body ? req.body : "");
+});
 app.use(express.json());
-app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'));
+app.use(
+  morgan(":method :url :status :res[content-length] - :response-time ms :body")
+);
 
-const errorHandler = (error,request,response,next) =>{
-  console.error(error.message)
+const errorHandler = (error, request, response, next) => {
+  console.error(error.message);
 
-  if (error.name === 'CastError') {
-    return response.status(400).send({ error: 'malformatted id' })
-  } 
+  if (error.name === "CastError") {
+    return response.status(400).send({ error: "malformatted id" });
+  }
 
-  next()
-}
+  next();
+};
 let persons = [
   {
     id: 1,
@@ -42,61 +46,76 @@ let persons = [
   },
 ];
 
-app.get("/persons",(request,response,next)=>{
-  Person.find({}).then(result => {
+app.get("/persons", (request, response, next) => {
+  Person.find({}).then((result) => {
     response.json(result);
-  })
+  });
 });
-app.delete("/persons/:id",(request,response,next)=>{
-  const id= request.params.id;
-  console.log("id is",id);
-  Person.findByIdAndDelete(id).then(result=>{
-    response.status(204).end();
+app.delete("/persons/:id", (request, response, next) => {
+  const id = request.params.id;
+  console.log("id is", id);
+  Person.findByIdAndDelete(id)
+    .then((result) => {
+      response.status(204).end();
+    })
+    .catch((error) => {
+      console.log(error);
+      next(error);
+    });
+});
+app.get("/persons/:id", (request, response, next) => {
+  const id = request.params.id;
+  Person.findById(id).then(result=>{
+    if(result)
+      {
+        response.json(result)
+      }
+      else
+      response.status(400).end();
   })
   .catch(error=>{
-    console.log(error)
     next(error);
   })
-})
-app.get("/persons/:id",(request,response,next)=>{
-  const id = Number(request.params.id);
-  const person = persons.find(person=>person.id ===id);
-  console.log(person);
-  if(person)
-    {
-  response.json(person);
-    }
-    else
-    response.status(400).send(`person not found`);
-})
-app.get("/info",(request,response,next)=>{
-  response.send(`<p>phonebook has ${persons.length} people</p><p>${new Date()}</p>`)
-})
+});
 
-app.post("/persons",(request,response,next)=>{
+app.put('/persons/:id', (request, response, next) => {
+  const body = request.body
+
+  const person = {
+    name: body.name,
+    number: body.number,
+  }
+
+  Person.findByIdAndUpdate(request.params.id, person, { new: true })
+    .then(updatedPerson => {
+      response.json(updatedPerson)
+    })
+    .catch(error => next(error))
+})
+app.get("/info", (request, response, next) => {
+  response.send(
+    `<p>phonebook has ${persons.length} people</p><p>${new Date()}</p>`
+  );
+});
+
+app.post("/persons", (request, response, next) => {
   const body = request.body;
   console.log(body);
-  if(!body.name || !body.number)
-    {
-      return response.status(400).json({
-        "error":"One or more property missing"
-      })
-    }
-    /* let id = Math.ceil(Math.random()*10000000000000);
-    const name = body.name;
-    if(persons.find(person=>person.name.toLowerCase()===name.toLowerCase()))
-      {
-        return response.status(400).json({"error":"name must be unique"});
-      } */
-    const person =new Person({name:body.name,number:body.number});
-    
-   person.save().then(savedPerson=>{
+  if (!body.name || !body.number) {
+    return response.status(400).json({
+      error: "One or more property missing",
+    });
+  }
+
+  const person = new Person({ name: body.name, number: body.number });
+
+  person.save().then((savedPerson) => {
     response.json(savedPerson);
-   })
-})
+  });
+});
 
 app.use(errorHandler);
 const PORT = process.env.PORT || 3001;
-app.listen(PORT,()=>{
-    console.log("server running....")
-})
+app.listen(PORT, () => {
+  console.log("server running....");
+});
